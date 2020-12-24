fd5ca0b5bd8840261e0b801c6313a21d9ff792bf
Aditya Galande <adityagalande14@gmail.com>
2020-12-24 12:31:35 +0530

    Event class initiated

---
+++ app/src/main/java/com/example/tsunami/Event.java
@@ -0,0 +1,14 @@
+package com.example.tsunami;
+
+public class Event {
+    public final String title;
+    public final long date;
+    public final int EventstunamiAlert;
+
+
+    public Event(String title, long date, int EventstunamiAlert) {
+        this.title = title;
+        this.date = date;
+        this.EventstunamiAlert = EventstunamiAlert;
+    }
+}
--- app/src/main/java/com/example/tsunami/MainActivity.java
+++ app/src/main/java/com/example/tsunami/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String USGS_URL = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2012-01-01&endtime=2012-12-01&minmagnitude=6";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
